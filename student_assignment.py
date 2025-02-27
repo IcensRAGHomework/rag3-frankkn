7ca4ca3da5cfdd584517d749b384766f1c2e8320
Frank Yang <frank840629@gmail.com>
2025-02-27 16:32:10 +0800

    test function 1

--- student_assignment.py
+++ student_assignment.py
@@ -29,34 +29,33 @@ def generate_hw01():
         deployment_id=gpt_emb_config['deployment_name']
     )
     
-    if collection.count() == 0:
-        # 建立或獲取 Collection
-        collection = chroma_client.get_or_create_collection(
-            name="TRAVEL", # 這是此 Collection 的名稱，用於標識此數據集的用途。
-            metadata={"hnsw:space": "cosine"}, # 這是設定查詢相似度計算的參數，cosine 表示使用餘弦相似度來進行距離計算。
-            embedding_function=openai_ef
-        )
-        
-        # 在初始化資料庫時，需從 CSV 檔案中提取每條記錄的相關欄位，並將其作為 Metadata 存入 ChromaDB
-        # 插入資料到 Collection
-        for _, row in df.iterrows():
-            metadata = {
-                "file_name": csv_file,
-                "name": row["Name"],
-                "type": row["Type"],
-                "address": row["Address"],
-                "tel": row["Tel"],
-                "city": row["City"],
-                "town": row["Town"],
-                "date": int(time.mktime(datetime.datetime.strptime(row["CreateDate"], "%Y-%m-%d").timetuple()))
-            }
-            
-            # 文件數據（documents） 將 CSV 檔案中的 HostWords 欄位內容提取作為文本數據存入 ChromaDB
-            # HostWords是查詢時進行相似度計算的核心。
-            document = row.get("HostWords", "") # 如果HostWords是null，則設為"" (空字串)
-            document_id = str(row["ID"])  
-            collection.add(ids=[document_id], documents=[document], metadatas=[metadata])
+    # 建立或獲取 Collection
+    collection = chroma_client.get_or_create_collection(
+        name="TRAVEL", # 這是此 Collection 的名稱，用於標識此數據集的用途。
+        metadata={"hnsw:space": "cosine"}, # 這是設定查詢相似度計算的參數，cosine 表示使用餘弦相似度來進行距離計算。
+        embedding_function=openai_ef
+    )
     
+    # 在初始化資料庫時，需從 CSV 檔案中提取每條記錄的相關欄位，並將其作為 Metadata 存入 ChromaDB
+    # 插入資料到 Collection
+    for _, row in df.iterrows():
+        metadata = {
+            "file_name": csv_file,
+            "name": row["Name"],
+            "type": row["Type"],
+            "address": row["Address"],
+            "tel": row["Tel"],
+            "city": row["City"],
+            "town": row["Town"],
+            "date": int(time.mktime(datetime.datetime.strptime(row["CreateDate"], "%Y-%m-%d").timestamp()))
+        }
+        
+        # 文件數據（documents） 將 CSV 檔案中的 HostWords 欄位內容提取作為文本數據存入 ChromaDB
+        # HostWords是查詢時進行相似度計算的核心。
+        document = row.get("HostWords", "") # 如果HostWords是null，則設為"" (空字串)
+        document_id = str(row["ID"])  
+        collection.add(ids=[document_id], documents=[document], metadatas=[metadata])
+
     return collection
     
 def generate_hw02(question, city, store_type, start_date, end_date):
