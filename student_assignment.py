a0ffbb63f28832254ab9dd4ca5d494ce2cf8c08b
Frank Yang <frank840629@gmail.com>
2025-02-27 17:43:34 +0800

    cp and debug

--- student_assignment.py
+++ student_assignment.py
@@ -12,31 +12,29 @@ gpt_emb_version = 'text-embedding-ada-002'
 gpt_emb_config = get_model_configuration(gpt_emb_version)
 
 dbpath = "./"
+csv_file = "COA_OpenData.csv"
 
 def generate_hw01():
-    csv_file = "COA_OpenData.csv"
-
-    # 讀取 CSV 檔案
-    df = pd.read_csv(csv_file)
-    
-    # 初始化 ChromaDB
-    chroma_client = chromadb.PersistentClient(path=dbpath)
     openai_ef = embedding_functions.OpenAIEmbeddingFunction(
-        api_key=gpt_emb_config['api_key'],
-        api_base=gpt_emb_config['api_base'],
-        api_type=gpt_emb_config['openai_type'],
-        api_version=gpt_emb_config['api_version'],
-        deployment_id=gpt_emb_config['deployment_name']
+        api_key = gpt_emb_config['api_key'],
+        api_base = gpt_emb_config['api_base'],
+        api_type = gpt_emb_config['openai_type'],
+        api_version = gpt_emb_config['api_version'],
+        deployment_id = gpt_emb_config['deployment_name']
     )
-    
-    # 建立或獲取 Collection
+
+    chroma_client = chromadb.PersistentClient(path = dbpath)
+
     collection = chroma_client.get_or_create_collection(
-        name="TRAVEL",
-        metadata={"hnsw:space": "cosine"},
-        embedding_function=openai_ef
+        name = "TRAVEL",
+        metadata = {"hnsw:space": "cosine"},
+        embedding_function = openai_ef
     )
-    if collection.count() == 0: 
-        for _, row in df.iterrows():
+
+    if collection.count() == 0:
+        data = pandas.read_csv(csv_file)
+        for index, row in data.iterrows():
+            id = str(row["ID"])
             metadata = {
                 "file_name": csv_file,
                 "name": row["Name"],
@@ -45,12 +43,11 @@ def generate_hw01():
                 "tel": row["Tel"],
                 "city": row["City"],
                 "town": row["Town"],
-                "date": int(datetime.datetime.strptime(row["CreateDate"], "%Y-%m-%d").timestamp())
+                "date": int(datetime.datetime.strptime(row["CreateDate"], '%Y-%m-%d').timestamp())
             }
-            
             document = row["HostWords"]
-            document_id = str(row["ID"])  
-            collection.add(ids=[document_id], documents=[document], metadatas=[metadata])
+
+            collection.add(ids = id, metadatas = metadata, documents = document)
 
     return collection
     
