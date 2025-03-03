d047d91e5bad9d7a5e9769b8529767626bdf6178
Frank Yang <frank840629@gmail.com>
2025-03-03 16:56:20 +0800

    test func 3

--- student_assignment.py
+++ student_assignment.py
@@ -80,7 +80,52 @@ def generate_hw02(question, city, store_type, start_date, end_date):
     return filtered_results
     
 def generate_hw03(question, store_name, new_store_name, city, store_type):
-    pass
+    collection = generate_hw01()
+
+     # 1. 更新指定店家的資訊
+    # 查詢 store_name 對應的店家
+    store_results = collection.query(query_texts=[store_name], n_results=1)
+    
+    if store_results["metadatas"] and store_results["metadatas"][0]:
+        metadata = store_results["metadatas"][0][0]
+        doc_id = store_results["ids"][0][0]
+        
+        # 更新 Metadata，加入 new_store_name
+        metadata["new_store_name"] = new_store_name
+        
+        # 刪除舊的，再新增更新後的
+        collection.delete(ids=[doc_id])
+        collection.add(ids=[doc_id], documents=[store_results["documents"][0][0]], metadatas=[metadata])
+    
+    # 2. 查詢符合條件的店家
+    query_results = collection.query(
+        query_texts=[question],
+        n_results=10,
+        where={
+            "$and": [
+                {"type": {"$in": store_type}},
+                {"city": {"$in": city}}
+            ]
+        }
+    )
+    
+    # 3. 篩選相似度 >= 0.80 的結果，並使用 new_store_name（如果存在）
+    metadatas = query_results['metadatas'][0]
+    distances = query_results['distances'][0]
+    
+    sorted_results = sorted(
+        zip(metadatas, distances),
+        key=lambda x: 1 - x[1],  # similarity = 1 - distance
+        reverse=True
+    )
+    
+    filtered_results = [
+        metadata.get("new_store_name", metadata["name"])  # 優先顯示 new_store_name
+        for metadata, distance in sorted_results if (1 - distance) >= 0.8
+    ]
+    
+    return filtered_results
+
     
 def demo(question):
     chroma_client = chromadb.PersistentClient(path=dbpath)
@@ -104,13 +149,21 @@ if __name__ == "__main__":
     # collection = demo(question)
     # print("Collection successfully created/retrieved:", collection.name)
 
-    generate_hw01()
+    # generate_hw01()
 
-    question = "我想要找有關茶餐點的店家"
-    city = ["宜蘭縣", "新北市"]
-    store_type = ["美食"]
-    start_date = datetime.datetime(2024, 4, 1)
-    end_date = datetime.datetime(2024, 5, 1)
+    # question = "我想要找有關茶餐點的店家"
+    # city = ["宜蘭縣", "新北市"]
+    # store_type = ["美食"]
+    # start_date = datetime.datetime(2024, 4, 1)
+    # end_date = datetime.datetime(2024, 5, 1)
     
-    ans_list = generate_hw02(question, city, store_type, start_date, end_date)
+    # ans_list = generate_hw02(question, city, store_type, start_date, end_date)
     # print(ans_list)
+
+    question = "我想要找南投縣的田媽媽餐廳，招牌是蕎麥麵"
+    store_name = "耄饕客棧"
+    new_store_name = "田媽媽（耄饕客棧）"
+    city = ["南投縣"]
+    store_type = ["美食"]
+    ans_list2 = generate_hw03(question, store_name, new_store_name, city, store_type)
+    print(ans_list2)
